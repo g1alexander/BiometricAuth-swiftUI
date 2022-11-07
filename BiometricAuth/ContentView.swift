39072bff70f477a3e16748a1a8c50404b2055f9b
g1alexander <alexlds26@gmail.com>
2022-11-07 14:26:18 -0500

    tutorial biometric auth

--- BiometricAuth/ContentView.swift
+++ BiometricAuth/ContentView.swift
@@ -6,16 +6,50 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    @State var auth = false
+    
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(auth ? "siuu" : "necesitas iniciar sesion")
+            
+            Button(auth ? "cerrar" : "authenticator"){
+                
+                if(auth){
+                    auth = false
+                }else{
+                    authentication()
+                }
+                
+            }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(auth ? Color.green : Color.yellow)
         .padding()
+    }
+    
+    private func authentication(){
+        
+        var error : NSError?
+        let laContext = LAContext()
+        
+        if
+            laContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            
+            laContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "utiliza sensor"){
+                autheticated, error in
+                
+                if autheticated{
+                    auth = true
+                }
+            }
+        }else{
+            auth = true
+        }
     }
 }
 
