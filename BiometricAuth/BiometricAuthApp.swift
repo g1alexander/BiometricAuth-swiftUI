43d34488549c609ed8e22c25f74eb70e75a60d0f
g1alexander <alexlds26@gmail.com>
2022-11-07 13:55:07 -0500

    Initial Commit

---
+++ BiometricAuth/BiometricAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthApp.swift
+//  BiometricAuth
+//
+//  Created by g1alexander on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
